c53d1fbcc260432c1a96d7815496b5de296c566e
Jianing Fan <jif59@pitt.edu>
2024-09-21 13:27:48 -0400

    Update make_set.py

--- make_set.py
+++ make_set.py
@@ -1,7 +1,7 @@
-def make_set(data)
-return_set = []
+def make_set(data):
+    return_set = []
 
-for item in make_set:
+for item in data:
     if item not in return_set:
         return_set.append(item)
 
