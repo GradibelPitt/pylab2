acdbcf6c2116ed580f711c1c9c4178365b329028
Jianing Fan <jif59@pitt.edu>
2024-09-21 13:19:23 -0400

    Update union.py

--- union.py
+++ union.py
@@ -1,10 +1,17 @@
 def union(setA, setB):
-   
-    if len(setA) != len(list(setA)) or len(setB) != len(list(setB)):
-        return []
-
-    result = setA[:]
-    for item in setB:
-        if item not in result:
+   for i in range(len(setA)):
+      if setA[i] in setA[:1]:
+            return []
+        
+   for i in range(len(setB)):
+      if setB[i] in setB[:i]:
+            return []
+           
+   result = setA[:]
+    
+   for item in setB:
+      if item not in result:
             result.append(item)
-    return result
+
+   return result
+  
